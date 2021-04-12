18f966928911145bcf9c4a2e0eff3ea4bad4bfef
BarboraVlkova <bvlkova@email.cz>
2021-04-12 14:42:55 +0200

    Prepracovany ukol, stale bez funkcniho zobrazeni na webu. :(

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/webStrankaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/webStrankaController.java
@@ -19,6 +19,39 @@ import java.util.stream.Collectors;
 @Controller
 public class webStrankaController {
 
+    Random nahodneCislo = new Random();
+    public webStrankaController(){
+        nahodneCislo = new Random();
+    }
+
+    @GetMapping("/")
+
+    public ModelAndView zmenaVzhleduStranky() {
+
+        ModelAndView result = new ModelAndView("webStranka");
+
+//  CITATY (v seznamu jich je 8)
+        int cisloCitatu = nahodneCislo.nextInt(7)+1;
+        List<String> seznamScitaty = Arrays.asList("„Všichni vědí naprosto přesně, jak mají žít druzí. Zato nikdy nevědí, jak mají žít oni sami.“ —  Paulo Coelho", "„Uprostřed problémů leží příležitost.“ —  Albert Einstein", "„Nechtěj být člověkem, který je úspěšný, ale člověkem, který za něco stojí.“ —  Albert Einstein", "„Ono je lepší mluvit s chytrým člověkem o něčem hloupém než s hlupákem o něčem chytrém.“ —  Jan Werich", "„Bylo by to k smíchu, kdyby to nebylo k pláči.“ —  Jan Werich", "„Vodu neoceníme, dokud nám nevyschne studna a to platí o všem v životě.“ —  Benjamin Franklin", "„Život bez zkoušek nestojí za žití.“ —  Sókratés", "„Odmítáme ty co nás milují, a milujeme ty co nás odmítají.“ —  Seneca");
+        String vyberCitat = seznamScitaty.get(cisloCitatu);
+
+//  OBRAZKY (v seznamu jich je 8)
+        int cisloObrazku = nahodneCislo.nextInt(7)+1;
+        List<String> seznamSobrazky = Arrays.asList("https://source.unsplash.com/DGFwQWKf-5o/1600x900","https://source.unsplash.com/5iW15BLz_2M/1600x900","https://source.unsplash.com/dx_6rdMmuzM/1600x900", "https://source.unsplash.com/DGFwQWKf-5o/1600x900", "https://source.unsplash.com/fkfKa0E1muw/1600x900", "https://source.unsplash.com/SPvJPDXEmqA/1600x900", "https://source.unsplash.com/Q_JjO7uaTh4/1600x900", "https://source.unsplash.com/vBos7bw98bY/1600x900");
+        String vyberObrazek = seznamSobrazky.get(cisloObrazku);
+
+//  VYTVORENI OBJEKTU, POJMENOVANI
+        result.addObject("vyberCitat",vyberCitat);
+        result.addObject("vyberObrazek",vyberObrazek);
+
+//  TO CO CHCI VRATIT
+        return result;
+    }
+
+
+
+//    Kód pro načtení řádků souboru v resourcech do seznamu řetězců (List<String>):
+//
 //    private static List<String> readAllLines(String resource)throws IOException {
 //        //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
 //        ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
@@ -35,25 +68,6 @@ public class webStrankaController {
 //                    .collect(Collectors.toList());
 //        }
 //    }
-
-
-    Random nahodneCislo = new Random();
-
-    @GetMapping("/")
-
-    public ModelAndView zmenaVzhleduStranky() {
-        ModelAndView result = new ModelAndView("webStranka");
-
-        List<String> citaty = Arrays.asList("„Všichni vědí naprosto přesně, jak mají žít druzí. Zato nikdy nevědí, jak mají žít oni sami.“ —  Paulo Coelho", "„Uprostřed problémů leží příležitost.“ —  Albert Einstein", "„Nechtěj být člověkem, který je úspěšný, ale člověkem, který za něco stojí.“ —  Albert Einstein", "„Ono je lepší mluvit s chytrým člověkem o něčem hloupém než s hlupákem o něčem chytrém.“ —  Jan Werich", "„Bylo by to k smíchu, kdyby to nebylo k pláči.“ —  Jan Werich", "„Vodu neoceníme, dokud nám nevyschne studna a to platí o všem v životě.“ —  Benjamin Franklin", "„Život bez zkoušek nestojí za žití.“ —  Sókratés", "„Odmítáme ty co nás milují, a milujeme ty co nás odmítají.“ —  Seneca");
-        int seznamScitaty = nahodneCislo.nextInt(citaty.size());
-        result.addObject("citaty", citaty.get(seznamScitaty));
-
-        List<String> obrazky = Arrays.asList("https://source.unsplash.com/DGFwQWKf-5o/1600x900","https://source.unsplash.com/5iW15BLz_2M/1600x900","https://source.unsplash.com/dx_6rdMmuzM/1600x900", "https://source.unsplash.com/DGFwQWKf-5o/1600x900", "https://source.unsplash.com/fkfKa0E1muw/1600x900", "https://source.unsplash.com/SPvJPDXEmqA/1600x900", "https://source.unsplash.com/Q_JjO7uaTh4/1600x900", "https://source.unsplash.com/vBos7bw98bY/1600x900");
-        int seznamSobrazky = nahodneCislo.nextInt(obrazky.size());
-        result.addObject("obrazky", obrazky.get(seznamSobrazky));
-        return result;
-    }
-
 
 
 }
